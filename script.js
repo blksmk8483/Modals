f7ac92710365674ffa5d9235845969162c492e1d
Brandon Knight <knightbrandon1984@gmail.com>
2023-09-10 23:49:51 -0500

    complete the modal

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 const openModal = function () {
-  console.log('Button clicked...');
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -22,3 +21,9 @@ const closeModal = function () {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
